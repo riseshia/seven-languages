e22999b484499a6c0bd3acc42a1c88574673f5bd
Shia <rise.shia@gmail.com>
2017-04-19 11:23:39 +0900

    Erlang - Day 1

---
+++ erlang/day-1.erl
@@ -0,0 +1,34 @@
+% This is a comment
+2 + 2.
+2 + 2.0.
+"String".
+[1, 2, 3].
+[72, 97, 32, 72, 97, 32, 72, 97].
+4 + "string". % Error
+variable = 4. % Error
+Var = 1.
+Var = 2. % Matching Error
+
+% 6.2.3
+red.
+Pill = blue.
+[1, 2, 3].
+[1, 2, "three"].
+List = [1, 2, 3].
+{one, two, three}.
+Origin = [0, 0].
+
+% 6.2.4
+Person = {person, {name, "Agent Smith"}, {profession, "Killing programs"}}
+{person, {name, Name}, {profession, Profession}} = Person
+
+[H|T] = [1, 2, 3].
+
+[X|Rest] = []. % Error
+
+W = 1.
+X = 2.
+Y = 3.
+Z = 4.
+All = <<W:3, X:3, Y:5, Z:5>>
+<<A:3, B:3, C:5, D:5>> = All
